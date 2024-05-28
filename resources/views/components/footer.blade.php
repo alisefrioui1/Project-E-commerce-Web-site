c05320fa902f9a6b25a81d49d330d175ad84c711
alisefrioui1 <alielsefriwi@gmail.com>
2024-05-28 16:10:02 +0100

    change on breeze

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>Made with</span>
+
+       
+
+        <span>by</span>
+
+        <a
+            href="https://github.com/Kamona-WD"
+            target="_blank"
+            class="text-blue-600 hover:underline"
+        >
+           Ali & Souad
+        </a>
+    </p>
+</footer>
